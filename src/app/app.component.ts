fda09448952829d905ba6131eab270b63a22ed20
ThippayawadeeS <Thippayawadees@gmail.com>
2021-02-27 14:09:47 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
-export class AppComponent {
-  title = 'product-lab4';
+export class AppComponent implements OnInit {
+  @ViewChild('productList',{static:true})
+  productList: ProductListComponent;
+
+ngOnInit(): void {
+  this.productList.products = [
+    {
+      name:'Orange',
+      price:111
+    },
+    {
+      name:'WaterMelon',
+      price:222
+    },
+    { name:'Coconut',
+      price:333
+    }
+  ];
 }
+constructor () {}
+}
+
